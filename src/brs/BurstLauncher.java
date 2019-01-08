4cd94fa02c79e74bfc1a5b02e2d8145be3521c15
Harry <37041552+harry1453@users.noreply.github.com>
2019-01-08 13:31:55 +0000

    Add classpath manipulation to launcher, not just GUI

--- src/brs/BurstLauncher.java
+++ src/brs/BurstLauncher.java
@@ -4,12 +4,27 @@ import org.slf4j.LoggerFactory;
 
 public class BurstLauncher {
     public static void main(String[] args) {
+        addToClasspath("./conf");
         try {
             Class.forName("javafx.application.Application");
             BurstGUI.main(args);
         } catch (ClassNotFoundException e) {
             LoggerFactory.getLogger(BurstLauncher.class).error("Could not start GUI as your JRE does not seem to have JavaFX installed. To install please install the \"openjfx\" package (eg. \"sudo apt install openjfx\")");
             Burst.main(args);
+        }
+    }
+
+    public static void addToClasspath(String path) {
+        try {
+            File f = new File(path);
+            URI u = f.toURI();
+            URLClassLoader urlClassLoader = (URLClassLoader) ClassLoader.getSystemClassLoader();
+            Class<URLClassLoader> urlClass = URLClassLoader.class;
+            Method method = urlClass.getDeclaredMethod("addURL", URL.class);
+            method.setAccessible(true);
+            method.invoke(urlClassLoader, u.toURL());
+        } catch (Exception e) {
+            LOGGER.error("Could not add path \"" + path + "\" to classpath", e);
         }
     }
 }

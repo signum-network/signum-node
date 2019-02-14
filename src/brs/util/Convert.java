054d11874480e8918da63937f25e19901cf4d85a
Harry Phillips <harryjamesphillips@gmail.com>
2019-02-14 21:28:59 +0000

    Use BC for hex encode / decode

--- src/brs/util/Convert.java
+++ src/brs/util/Convert.java
@@ -3,6 +3,8 @@ package brs.util;
 import brs.BurstException;
 import brs.Constants;
 import brs.crypto.Crypto;
+import org.bouncycastle.util.encoders.DecoderException;
+import org.bouncycastle.util.encoders.Hex;
 
 import java.math.BigInteger;
 import java.nio.ByteBuffer;
@@ -12,7 +14,6 @@ import java.util.Date;
 
 public final class Convert {
 
-  private static final char[] hexChars = { '0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f' };
   private static final long[] multipliers = {1, 10, 100, 1000, 10000, 100000, 1000000, 10000000, 100000000};
 
   public static final BigInteger two64 = new BigInteger("18446744073709551616");
@@ -20,33 +21,18 @@ public final class Convert {
   private Convert() {} //never
 
   public static byte[] parseHexString(String hex) {
-    if (hex == null) {
-      return null;
-    }
-    byte[] bytes = new byte[hex.length() / 2];
-    for (int i = 0; i < bytes.length; i++) {
-      int char1 = hex.charAt(i * 2);
-      char1 = char1 > 0x60 ? char1 - 0x57 : char1 - 0x30;
-      int char2 = hex.charAt(i * 2 + 1);
-      char2 = char2 > 0x60 ? char2 - 0x57 : char2 - 0x30;
-      if (char1 < 0 || char2 < 0 || char1 > 15 || char2 > 15) {
-        throw new NumberFormatException("Invalid hex number: " + hex);
+    try {
+      if (hex.length() % 2 != 0) {
+        hex = hex.substring(0, hex.length() - 1);
       }
-      bytes[i] = (byte)((char1 << 4) + char2);
+      return Hex.decode(hex);
+    } catch (DecoderException e) {
+      throw new RuntimeException("Could not parse hex string " + hex, e);
     }
-    return bytes;
   }
 
   public static String toHexString(byte[] bytes) {
-    if (bytes == null) {
-      return null;
-    }
-    char[] chars = new char[bytes.length * 2];
-    for (int i = 0; i < bytes.length; i++) {
-      chars[i * 2] = hexChars[((bytes[i] >> 4) & 0xF)];
-      chars[i * 2 + 1] = hexChars[(bytes[i] & 0xF)];
-    }
-    return String.valueOf(chars);
+    return Hex.toHexString(bytes);
   }
 
   public static String toUnsignedLong(long objectId) {

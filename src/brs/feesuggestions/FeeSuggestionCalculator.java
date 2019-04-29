0a8153dc38a1553430f490c579281d6221c63727
Harry Phillips <harryjamesphillips@gmail.com>
2019-04-29 10:06:37 +0100

    Remove commons collections type from fee suggestion calculator

--- src/brs/feesuggestions/FeeSuggestionCalculator.java
+++ src/brs/feesuggestions/FeeSuggestionCalculator.java
@@ -4,28 +4,28 @@ import brs.Block;
 import brs.BlockchainProcessor;
 import brs.BlockchainProcessor.Event;
 import brs.db.store.BlockchainStore;
-import org.apache.commons.collections4.queue.CircularFifoQueue;
+
+import java.util.Arrays;
 
 import static brs.Constants.FEE_QUANT;
 
 public class FeeSuggestionCalculator {
 
-  private final CircularFifoQueue<Block> latestBlocks;
+  // index 0 = oldest, length-1 = newest
+  private final Block[] latestBlocks;
 
   private final BlockchainStore blockchainStore;
 
   private FeeSuggestion feeSuggestion;
 
   public FeeSuggestionCalculator(BlockchainProcessor blockchainProcessor, BlockchainStore blockchainStore, int historyLength) {
-    this.latestBlocks = new CircularFifoQueue<>(historyLength);
-
+    latestBlocks = new Block[historyLength];
     this.blockchainStore = blockchainStore;
-
     blockchainProcessor.addListener(this::newBlockApplied, Event.AFTER_BLOCK_APPLY);
   }
 
   public FeeSuggestion giveFeeSuggestion() {
-    if (latestBlocks.isEmpty()) {
+    if (latestBlocksIsEmpty()) {
       fillInitialHistory();
       recalculateSuggestion();
     }
@@ -34,22 +34,37 @@ public class FeeSuggestionCalculator {
   }
 
   private void newBlockApplied(Block block) {
-    if (latestBlocks.isEmpty()) {
+    if (latestBlocksIsEmpty()) {
       fillInitialHistory();
     }
 
-    this.latestBlocks.add(block);
+    pushNewBlock(block);
     recalculateSuggestion();
   }
 
   private void fillInitialHistory() {
-    blockchainStore.getLatestBlocks(latestBlocks.maxSize()).forEachRemaining(latestBlocks::add);
+    blockchainStore.getLatestBlocks(latestBlocks.length).forEachRemaining(this::pushNewBlock);
+  }
+
+  private boolean latestBlocksIsEmpty() {
+    for (Block latestBlock : latestBlocks) {
+      if (latestBlock == null) return true;
+    }
+    return false;
+  }
+
+  private void pushNewBlock(Block block) {
+    // Skip index 0 as we want to remove this one
+    for (int i = 1; i < latestBlocks.length; i++) {
+      latestBlocks[i-1] = latestBlocks[i];
+    }
+    latestBlocks[latestBlocks.length - 1] = block;
   }
 
   private void recalculateSuggestion() {
-    int lowestAmountTransactionsNearHistory = latestBlocks.stream().mapToInt(b -> b.getTransactions().size()).min().orElse(1);
-    int averageAmountTransactionsNearHistory = (int) Math.ceil(latestBlocks.stream().mapToInt(b -> b.getTransactions().size()).average().orElse(1));
-    int highestAmountTransactionsNearHistory = latestBlocks.stream().mapToInt(b -> b.getTransactions().size()).max().orElse(1);
+    int lowestAmountTransactionsNearHistory = Arrays.stream(latestBlocks).mapToInt(b -> b.getTransactions().size()).min().orElse(1);
+    int averageAmountTransactionsNearHistory = (int) Math.ceil(Arrays.stream(latestBlocks).mapToInt(b -> b.getTransactions().size()).average().orElse(1));
+    int highestAmountTransactionsNearHistory = Arrays.stream(latestBlocks).mapToInt(b -> b.getTransactions().size()).max().orElse(1);
 
     long cheapFee = (1 + lowestAmountTransactionsNearHistory) * FEE_QUANT;
     long standardFee = (1 + averageAmountTransactionsNearHistory) * FEE_QUANT;

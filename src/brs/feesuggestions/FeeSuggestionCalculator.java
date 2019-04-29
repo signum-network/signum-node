a0bf3b717cb0c342745741dcbb0f1b9f3a1008ee
Harry Phillips <harryjamesphillips@gmail.com>
2019-04-29 10:22:15 +0100

    Fix issues in fee suggestion calculator

--- src/brs/feesuggestions/FeeSuggestionCalculator.java
+++ src/brs/feesuggestions/FeeSuggestionCalculator.java
@@ -16,7 +16,7 @@ public class FeeSuggestionCalculator {
 
   private final BlockchainStore blockchainStore;
 
-  private FeeSuggestion feeSuggestion;
+  private FeeSuggestion feeSuggestion = new FeeSuggestion(FEE_QUANT, FEE_QUANT, FEE_QUANT);
 
   public FeeSuggestionCalculator(BlockchainProcessor blockchainProcessor, BlockchainStore blockchainStore, int historyLength) {
     latestBlocks = new Block[historyLength];
@@ -54,22 +54,24 @@ public class FeeSuggestionCalculator {
   }
 
   private void pushNewBlock(Block block) {
+    if (block == null) return;
     // Skip index 0 as we want to remove this one
-    for (int i = 1; i < latestBlocks.length; i++) {
-      latestBlocks[i-1] = latestBlocks[i];
-    }
+    if (latestBlocks.length - 1 >= 0) System.arraycopy(latestBlocks, 1, latestBlocks, 0, latestBlocks.length - 1);
     latestBlocks[latestBlocks.length - 1] = block;
   }
 
   private void recalculateSuggestion() {
-    int lowestAmountTransactionsNearHistory = Arrays.stream(latestBlocks).mapToInt(b -> b.getTransactions().size()).min().orElse(1);
-    int averageAmountTransactionsNearHistory = (int) Math.ceil(Arrays.stream(latestBlocks).mapToInt(b -> b.getTransactions().size()).average().orElse(1));
-    int highestAmountTransactionsNearHistory = Arrays.stream(latestBlocks).mapToInt(b -> b.getTransactions().size()).max().orElse(1);
+    try {
+      int lowestAmountTransactionsNearHistory = Arrays.stream(latestBlocks).mapToInt(b -> b.getTransactions().size()).min().orElse(1);
+      int averageAmountTransactionsNearHistory = (int) Math.ceil(Arrays.stream(latestBlocks).mapToInt(b -> b.getTransactions().size()).average().orElse(1));
+      int highestAmountTransactionsNearHistory = Arrays.stream(latestBlocks).mapToInt(b -> b.getTransactions().size()).max().orElse(1);
 
-    long cheapFee = (1 + lowestAmountTransactionsNearHistory) * FEE_QUANT;
-    long standardFee = (1 + averageAmountTransactionsNearHistory) * FEE_QUANT;
-    long priorityFee = (1 + highestAmountTransactionsNearHistory) * FEE_QUANT;
+      long cheapFee = (1 + lowestAmountTransactionsNearHistory) * FEE_QUANT;
+      long standardFee = (1 + averageAmountTransactionsNearHistory) * FEE_QUANT;
+      long priorityFee = (1 + highestAmountTransactionsNearHistory) * FEE_QUANT;
 
-    feeSuggestion = new FeeSuggestion(cheapFee, standardFee, priorityFee);
+      feeSuggestion = new FeeSuggestion(cheapFee, standardFee, priorityFee);
+    } catch (NullPointerException ignored) { // Can happen if there are less than latestBlocks.length blocks in the blockchain
+    }
   }
 }

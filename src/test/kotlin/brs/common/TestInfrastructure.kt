36aa3b12fb22bc4c1e8f8e5f67a504064846f7b7
Harry Phillips <harryjamesphillips@gmail.com>
2019-11-14 19:45:19 +0000

    Fix H2

--- src/test/kotlin/brs/common/TestInfrastructure.kt
+++ src/test/kotlin/brs/common/TestInfrastructure.kt
@@ -1,7 +1,7 @@
 package brs.common
 
 object TestInfrastructure {
-    const val IN_MEMORY_H2_DB_URL = "jdbc:h2:mem;DB_CLOSE_ON_EXIT=FALSE"
+    const val IN_MEMORY_H2_DB_URL = "jdbc:h2:mem:test;DB_CLOSE_ON_EXIT=FALSE"
     const val IN_MEMORY_SQLITE_DB_URL = "jdbc:sqlite::memory:"
 
     const val TEST_API_PORT = 8885

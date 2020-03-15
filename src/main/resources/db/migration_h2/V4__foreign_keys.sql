f047cd77aea7f335f7b029b5af1716558a34896a
Lev Lybin <lev.lybin@gmail.com>
2020-03-16 02:10:33 +0700

    foreign keys

---
+++ src/main/resources/db/migration_h2/V4__foreign_keys.sql
@@ -0,0 +1,250 @@
+-- account
+
+-- h2 required not composite key for link foreign key
+create index ACCOUNT_ID_INDEX
+	on "account" ("id");
+
+-- account_asset
+
+alter table "account_asset"
+	add constraint ACCOUNT_ASSET_ACCOUNT_ID_FK
+		foreign key ("account_id") references "account" ("id")
+			on delete cascade;
+
+alter table "account_asset"
+	add constraint ACCOUNT_ASSET_ASSET_ID_FK
+		foreign key ("asset_id") references "asset" ("id")
+			on delete cascade;
+
+-- alias
+
+alter table "alias"
+	add constraint ALIAS_ACCOUNT_ID_FK
+		foreign key ("account_id") references "account" ("id")
+			on delete cascade;
+
+-- alias_offer
+
+alter table "alias_offer"
+	add constraint ALIAS_OFFER_ACCOUNT_ID_FK
+		foreign key ("buyer_id") references "account" ("id")
+			on delete cascade;
+
+-- ask_order
+
+alter table "ask_order"
+	add constraint ASK_ORDER_ACCOUNT_ID_FK
+		foreign key ("account_id") references "account" ("id")
+			on delete cascade;
+
+alter table "ask_order"
+	add constraint ASK_ORDER_ASSET_ID_FK
+		foreign key ("asset_id") references "asset" ("id")
+			on delete cascade;
+
+alter table ask_order
+	add constraint ask_order_asset_id_fk
+		foreign key (asset_id) references asset (id)
+			on delete cascade;
+
+-- asset
+
+alter table "asset"
+	add constraint ASSET_ASSET_ID_FK
+		foreign key ("account_id") references "asset" ("id")
+			on delete cascade;
+
+-- asset_transfer
+
+alter table "asset_transfer"
+	add constraint ASSET_TRANSFER_ACCOUNT_ID_FK
+		foreign key ("sender_id") references "account" ("id")
+			on delete cascade;
+
+alter table "asset_transfer"
+	add constraint ASSET_TRANSFER_ACCOUNT_ID_FK_2
+		foreign key ("recipient_id") references "account" ("id")
+			on delete cascade;
+
+alter table "asset_transfer"
+	add constraint ASSET_TRANSFER_ASSET_ID_FK
+		foreign key ("asset_id") references "asset" ("id")
+			on delete cascade;
+
+-- at
+
+alter table "at"
+	add constraint AT_ACCOUNT_ID_FK
+		foreign key ("creator_id") references "account" ("id")
+			on delete cascade;
+
+-- at_state
+
+alter table "at_state"
+	add constraint AT_STATE_AT_ID_FK
+		foreign key ("at_id") references "at" ("id")
+			on delete cascade;
+
+-- bid_order
+
+alter table "bid_order"
+	add constraint BID_ORDER_ACCOUNT_ID_FK
+		foreign key ("account_id") references "account" ("id")
+			on delete cascade;
+
+alter table "bid_order"
+	add constraint BID_ORDER_ASSET_ID_FK
+		foreign key ("asset_id") references "asset" ("id")
+			on delete cascade;
+
+-- block
+-- no generator_id foreign key because account may be not yet created
+
+-- escrow
+
+alter table "escrow"
+	add constraint ESCROW_ACCOUNT_ID_FK
+		foreign key ("sender_id") references "account" ("id")
+			on delete cascade;
+
+alter table "escrow"
+	add constraint ESCROW_ACCOUNT_ID_FK_2
+		foreign key ("recipient_id") references "account" ("id")
+			on delete cascade;
+
+-- escrow_decision
+
+alter table "escrow_decision"
+	add constraint ESCROW_DECISION_ACCOUNT_ID_FK
+		foreign key ("account_id") references "account" ("id")
+			on delete cascade;
+
+alter table "escrow_decision"
+	add constraint ESCROW_DECISION_ESCROW_ID_FK
+		foreign key ("escrow_id") references "escrow" ("id")
+			on delete cascade;
+
+-- goods
+
+alter table "goods"
+	add constraint GOODS_ACCOUNT_ID_FK
+		foreign key ("seller_id") references "account" ("id")
+			on delete cascade;
+
+-- indirect_incoming
+
+alter table "indirect_incoming"
+	add constraint INDIRECT_INCOMING_ACCOUNT_ID_FK
+		foreign key ("account_id") references "account" ("id")
+			on delete cascade;
+
+alter table "indirect_incoming"
+	add constraint INDIRECT_INCOMING_TRANSACTION_ID_FK
+		foreign key ("transaction_id") references "transaction" ("id")
+			on delete cascade;
+
+-- purchase
+
+alter table "purchase"
+	add constraint PURCHASE_ACCOUNT_ID_FK
+		foreign key ("buyer_id") references "account" ("id")
+			on delete cascade;
+
+alter table "purchase"
+	add constraint PURCHASE_ACCOUNT_ID_FK_2
+		foreign key ("seller_id") references "account" ("id")
+			on delete cascade;
+
+alter table "purchase"
+	add constraint PURCHASE_GOODS_ID_FK
+		foreign key ("goods_id") references "goods" ("id")
+			on delete cascade;
+
+-- purchase_feedback
+-- TODO: what is purchase_feedback.id? purchase.id?
+
+-- purchase_public_feedback
+-- TODO: what is purchase_public_feedback.id? purchase.id?
+
+-- reward_recip_assign
+
+alter table "reward_recip_assign"
+	add constraint REWARD_RECIP_ASSIGN_ACCOUNT_ID_FK
+		foreign key ("account_id") references "account" ("id")
+			on delete cascade;
+
+alter table "reward_recip_assign"
+	add constraint REWARD_RECIP_ASSIGN_ACCOUNT_ID_FK_2
+		foreign key ("prev_recip_id") references "account" ("id")
+			on delete cascade;
+
+alter table "reward_recip_assign"
+	add constraint REWARD_RECIP_ASSIGN_ACCOUNT_ID_FK_3
+		foreign key ("recip_id") references "account" ("id")
+			on delete cascade;
+
+-- subscription
+
+alter table "subscription"
+	add constraint SUBSCRIPTION_ACCOUNT_ID_FK
+		foreign key ("sender_id") references "account" ("id")
+			on delete cascade;
+
+alter table "subscription"
+	add constraint SUBSCRIPTION_ACCOUNT_ID_FK_2
+		foreign key ("recipient_id") references "account" ("id")
+			on delete cascade;
+
+-- trade
+
+alter table "trade"
+	add constraint TRADE_ACCOUNT_ID_FK
+		foreign key ("seller_id") references "account" ("id")
+			on delete cascade;
+
+alter table "trade"
+	add constraint TRADE_ACCOUNT_ID_FK_2
+		foreign key ("buyer_id") references "account" ("id")
+			on delete cascade;
+
+alter table "trade"
+	add constraint TRADE_ASK_ORDER_ID_FK
+		foreign key ("ask_order_id") references "ask_order" ("id")
+			on delete cascade;
+
+alter table "trade"
+	add constraint TRADE_ASSET_ID_FK
+		foreign key ("asset_id") references "asset" ("id")
+			on delete cascade;
+
+alter table "trade"
+	add constraint TRADE_BID_ORDER_ID_FK
+		foreign key ("bid_order_id") references "bid_order" ("id")
+			on delete cascade;
+
+alter table "trade"
+	add constraint TRADE_BLOCK_ID_FK
+		foreign key ("block_id") references "block" ("id")
+			on delete cascade;
+
+-- transaction
+
+alter table "transaction"
+	add constraint TRANSACTION_ACCOUNT_ID_FK
+		foreign key ("recipient_id") references "account" ("id")
+			on delete cascade;
+
+alter table "transaction"
+	add constraint TRANSACTION_ACCOUNT_ID_FK_2
+		foreign key ("sender_id") references "account" ("id")
+			on delete cascade;
+
+alter table "transaction"
+	add constraint TRANSACTION_BLOCK_ID_FK
+		foreign key ("block_id") references "block" ("id")
+			on delete cascade;
+
+alter table "transaction"
+	add constraint TRANSACTION_BLOCK_ID_FK_2
+		foreign key ("ec_block_id") references "block" ("id")
+			on delete cascade;
---
+++ src/main/resources/db/migration_mariadb/V4__foreign_keys.sql
@@ -0,0 +1,425 @@
+-- Descending index will give effect only for MySQL/Oracle, maybe for MariaDB in future
+-- https://jira.mariadb.org/browse/MDEV-13756
+
+-- TODO: for height fg?
+
+-- account
+
+drop index account_id_balance_height_idx on account;
+create index account_id_balance_height_idx
+	on account (id asc, balance asc, height desc);
+
+drop index account_id_height_idx on account;
+create unique index account_id_height_idx
+	on account (id asc, height desc);
+
+drop index account_asset_id_height_idx on account_asset;
+create unique index account_asset_id_height_idx
+	on account_asset (account_id asc, asset_id asc, height desc);
+
+drop index account_asset_quantity_idx on account_asset;
+create index account_asset_quantity_idx
+	on account_asset (quantity desc);
+
+-- account_asset
+
+alter table account_asset
+	add constraint account_asset_account_id_fk
+		foreign key (account_id) references account (id)
+			on delete cascade;
+
+alter table account_asset
+	add constraint account_asset_asset_id_fk
+		foreign key (asset_id) references asset (id)
+			on delete cascade;
+
+-- alias
+
+drop index alias_account_id_idx on alias;
+create index alias_account_id_idx
+	on alias (account_id asc, height desc);
+
+drop index alias_id_height_idx on alias;
+create unique index alias_id_height_idx
+	on alias (id asc, height desc);
+
+alter table alias
+	add constraint alias_account_id_fk
+		foreign key (account_id) references account (id)
+			on delete cascade;
+
+-- alias_offer
+
+drop index alias_offer_id_height_idx on alias_offer;
+create unique index alias_offer_id_height_idx
+	on alias_offer (id asc, height desc);
+
+alter table alias_offer
+	add constraint alias_offer_account_id_fk
+		foreign key (buyer_id) references account (id)
+			on delete cascade;
+
+-- ask_order
+
+drop index ask_order_account_id_idx on ask_order;
+create index ask_order_account_id_idx
+	on ask_order (account_id asc, height desc);
+
+drop index ask_order_creation_idx on ask_order;
+create index ask_order_creation_idx
+	on ask_order (creation_height desc);
+
+drop index ask_order_id_height_idx on ask_order;
+create unique index ask_order_id_height_idx
+	on ask_order (id asc, height desc);
+
+alter table ask_order
+	add constraint ask_order_account_id_fk
+		foreign key (account_id) references account (id)
+			on delete cascade;
+
+-- asset
+
+alter table asset
+	add constraint asset_account_id_fk
+		foreign key (account_id) references account (id)
+			on delete cascade;
+
+-- asset_transfer
+
+drop index asset_transfer_asset_id_idx on asset_transfer;
+create index asset_transfer_asset_id_idx
+	on asset_transfer (asset_id asc, height desc);
+
+drop index asset_transfer_recipient_id_idx on asset_transfer;
+create index asset_transfer_recipient_id_idx
+	on asset_transfer (recipient_id asc, height desc);
+
+drop index asset_transfer_sender_id_idx on asset_transfer;
+create index asset_transfer_sender_id_idx
+	on asset_transfer (sender_id asc, height desc);
+
+alter table asset_transfer
+	add constraint asset_transfer_account_id_fk
+		foreign key (sender_id) references account (id)
+			on delete cascade;
+
+alter table asset_transfer
+	add constraint asset_transfer_account_id_fk_2
+		foreign key (recipient_id) references account (id)
+			on delete cascade;
+
+alter table asset_transfer
+	add constraint asset_transfer_asset_id_fk
+		foreign key (asset_id) references asset (id)
+			on delete cascade;
+
+-- at
+
+drop index at_creator_id_height_idx on at;
+create index at_creator_id_height_idx
+	on at (creator_id asc, height desc);
+
+drop index at_id_height_idx on at;
+create unique index at_id_height_idx
+	on at (id asc, height desc);
+
+alter table at
+	add constraint at_account_id_fk
+		foreign key (creator_id) references account (id)
+			on delete cascade;
+
+-- at_state
+
+drop index at_state_at_id_height_idx on at_state;
+create unique index at_state_at_id_height_idx
+	on at_state (at_id asc, height desc);
+
+drop index at_state_id_next_height_height_idx on at_state;
+create index at_state_id_next_height_height_idx
+	on at_state (at_id asc, next_height asc, height desc);
+
+alter table at_state
+	add constraint at_state_at_id_fk
+		foreign key (at_id) references at (id)
+			on delete cascade;
+
+-- bid_order
+
+drop index bid_order_account_id_idx on bid_order;
+create index bid_order_account_id_idx
+	on bid_order (account_id asc, height desc);
+
+drop index bid_order_creation_idx on bid_order;
+create index bid_order_creation_idx
+	on bid_order (creation_height desc);
+
+drop index bid_order_id_height_idx on bid_order;
+create unique index bid_order_id_height_idx
+	on bid_order (id asc, height desc);
+
+alter table bid_order
+	add constraint bid_order_account_id_fk
+		foreign key (account_id) references account (id)
+			on delete cascade;
+
+alter table bid_order
+	add constraint bid_order_asset_id_fk
+		foreign key (asset_id) references asset (id)
+			on delete cascade;
+
+-- block
+-- no generator_id foreign key because account may be not yet created
+
+drop index block_timestamp_idx on block;
+create unique index block_timestamp_idx
+	on block (timestamp desc);
+
+-- escrow
+
+drop index escrow_deadline_height_idx on escrow;
+create index escrow_deadline_height_idx
+	on escrow (deadline asc, height desc);
+
+drop index escrow_id_height_idx on escrow;
+create unique index escrow_id_height_idx
+	on escrow (id asc, height desc);
+
+drop index escrow_recipient_id_height_idx on escrow;
+create index escrow_recipient_id_height_idx
+	on escrow (recipient_id asc, height desc);
+
+drop index escrow_sender_id_height_idx on escrow;
+create index escrow_sender_id_height_idx
+	on escrow (sender_id asc, height desc);
+
+alter table escrow
+	add constraint escrow_account_id_fk
+		foreign key (sender_id) references account (id)
+			on delete cascade;
+
+alter table escrow
+	add constraint escrow_account_id_fk_2
+		foreign key (recipient_id) references account (id)
+			on delete cascade;
+
+-- escrow_decision
+
+drop index escrow_decision_account_id_height_idx on escrow_decision;
+create index escrow_decision_account_id_height_idx
+	on escrow_decision (account_id asc, height desc);
+
+drop index escrow_decision_escrow_id_account_id_height_idx on escrow_decision;
+create unique index escrow_decision_escrow_id_account_id_height_idx
+	on escrow_decision (escrow_id asc, account_id asc, height desc);
+
+drop index escrow_decision_escrow_id_height_idx on escrow_decision;
+create index escrow_decision_escrow_id_height_idx
+	on escrow_decision (escrow_id asc, height desc);
+
+alter table escrow_decision
+	add constraint escrow_decision_account_id_fk
+		foreign key (account_id) references account (id)
+			on delete cascade;
+
+alter table escrow_decision
+	add constraint escrow_decision_escrow_id_fk
+		foreign key (escrow_id) references escrow (id)
+			on delete cascade;
+
+-- goods
+
+drop index goods_id_height_idx on goods;
+create unique index goods_id_height_idx
+	on goods (id asc, height desc);
+
+drop index goods_timestamp_idx on goods;
+create index goods_timestamp_idx
+	on goods (timestamp desc, height desc);
+
+alter table goods
+	add constraint goods_account_id_fk
+		foreign key (seller_id) references account (id)
+			on delete cascade;
+
+-- indirect_incoming
+
+alter table indirect_incoming
+	add constraint indirect_incoming_account_id_fk
+		foreign key (account_id) references account (id)
+			on delete cascade;
+
+alter table indirect_incoming
+	add constraint indirect_incoming_transaction_id_fk
+		foreign key (transaction_id) references transaction (id)
+			on delete cascade;
+
+-- purchase
+
+drop index purchase_buyer_id_height_idx on purchase;
+create index purchase_buyer_id_height_idx
+	on purchase (buyer_id asc, height desc);
+
+drop index purchase_deadline_idx on purchase;
+create index purchase_deadline_idx
+	on purchase (deadline asc, height desc);
+
+drop index purchase_id_height_idx on purchase;
+create unique index purchase_id_height_idx
+	on purchase (id asc, height desc);
+
+drop index purchase_seller_id_height_idx on purchase;
+create index purchase_seller_id_height_idx
+	on purchase (seller_id asc, height desc);
+
+drop index purchase_timestamp_idx on purchase;
+create index purchase_timestamp_idx
+	on purchase (timestamp desc, id asc);
+
+alter table purchase
+	add constraint purchase_account_id_fk
+		foreign key (buyer_id) references account (id)
+			on delete cascade;
+
+alter table purchase
+	add constraint purchase_account_id_fk_2
+		foreign key (seller_id) references account (id)
+			on delete cascade;
+
+alter table purchase
+	add constraint purchase_goods_id_fk
+		foreign key (goods_id) references goods (id)
+			on delete cascade;
+
+-- purchase_feedback
+-- TODO: what is purchase_feedback.id? purchase.id?
+
+drop index purchase_feedback_id_height_idx on purchase_feedback;
+create index purchase_feedback_id_height_idx
+	on purchase_feedback (id asc, height desc);
+
+-- purchase_public_feedback
+-- TODO: what is purchase_public_feedback.id? purchase.id?
+
+drop index purchase_public_feedback_id_height_idx on purchase_public_feedback;
+create index purchase_public_feedback_id_height_idx
+	on purchase_public_feedback (id asc, height desc);
+
+-- reward_recip_assign
+
+drop index reward_recip_assign_account_id_height_idx on reward_recip_assign;
+create unique index reward_recip_assign_account_id_height_idx
+	on reward_recip_assign (account_id asc, height desc);
+
+drop index reward_recip_assign_recip_id_height_idx on reward_recip_assign;
+create index reward_recip_assign_recip_id_height_idx
+	on reward_recip_assign (recip_id asc, height desc);
+
+alter table reward_recip_assign
+	add constraint reward_recip_assign_account_id_fk
+		foreign key (account_id) references account (id)
+			on delete cascade;
+
+alter table reward_recip_assign
+	add constraint reward_recip_assign_account_id_fk_2
+		foreign key (prev_recip_id) references account (id)
+			on delete cascade;
+
+alter table reward_recip_assign
+	add constraint reward_recip_assign_account_id_fk_3
+		foreign key (recip_id) references account (id)
+			on delete cascade;
+
+-- subscription
+
+drop index subscription_id_height_idx on subscription;
+create unique index subscription_id_height_idx
+	on subscription (id asc, height desc);
+
+drop index subscription_recipient_id_height_idx on subscription;
+create index subscription_recipient_id_height_idx
+	on subscription (recipient_id asc, height desc);
+
+drop index subscription_sender_id_height_idx on subscription;
+create index subscription_sender_id_height_idx
+	on subscription (sender_id asc, height desc);
+
+alter table subscription
+	add constraint subscription_account_id_fk
+		foreign key (sender_id) references account (id)
+			on delete cascade;
+
+alter table subscription
+	add constraint subscription_account_id_fk_2
+		foreign key (recipient_id) references account (id)
+			on delete cascade;
+
+-- trade
+
+drop index trade_asset_id_idx on trade;
+create index trade_asset_id_idx
+	on trade (asset_id asc, height desc);
+
+drop index trade_buyer_id_idx on trade;
+create index trade_buyer_id_idx
+	on trade (buyer_id asc, height desc);
+
+drop index trade_seller_id_idx on trade;
+create index trade_seller_id_idx
+	on trade (seller_id asc, height desc);
+
+	alter table trade
+	add constraint trade_account_id_fk
+		foreign key (seller_id) references account (id)
+			on delete cascade;
+
+alter table trade
+	add constraint trade_account_id_fk_2
+		foreign key (buyer_id) references account (id)
+			on delete cascade;
+
+alter table trade
+	add constraint trade_ask_order_id_fk
+		foreign key (ask_order_id) references ask_order (id)
+			on delete cascade;
+
+alter table trade
+	add constraint trade_asset_id_fk
+		foreign key (asset_id) references asset (id)
+			on delete cascade;
+
+alter table trade
+	add constraint trade_bid_order_id_fk
+		foreign key (bid_order_id) references bid_order (id)
+			on delete cascade;
+
+alter table trade
+	add constraint trade_block_id_fk
+		foreign key (block_id) references block (id)
+			on delete cascade;
+
+-- transaction
+
+drop index transaction_block_timestamp_idx on transaction;
+create index transaction_block_timestamp_idx
+	on transaction (block_timestamp desc);
+
+alter table transaction
+	add constraint transaction_account_id_fk
+		foreign key (recipient_id) references account (id)
+			on delete cascade;
+
+alter table transaction
+	add constraint transaction_account_id_fk_2
+		foreign key (sender_id) references account (id)
+			on delete cascade;
+
+alter table transaction
+	add constraint transaction_block_id_fk
+		foreign key (block_id) references block (id)
+			on delete cascade;
+
+alter table transaction
+	add constraint transaction_block_id_fk_2
+		foreign key (ec_block_id) references block (id)
+			on delete cascade;

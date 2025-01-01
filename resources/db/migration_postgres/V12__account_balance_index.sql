95d5f5cbe6e8643e453fb871206a30433e635358
frankTheTank72 <39116221+frankTheTank72@users.noreply.github.com>
2025-01-01 15:35:43 +0100

    Small change for postgres

--- resources/db/migration_postgres/V12__account_balance_index.sql
+++ resources/db/migration_postgres/V12__account_balance_index.sql
@@ -1,2 +1,2 @@
-DROP INDEX IF EXISTS aaccount_asset_assetid_idx ON account_asset;
+DROP INDEX IF EXISTS account_asset_assetid_idx;
 CREATE INDEX IF NOT EXISTS account_asset_assetid_latest_idx ON account_asset (account_id, asset_id, latest);

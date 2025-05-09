e12427285e9a3e83ada61f2516589cdf2b2e64b8
damccull <401104+damccull@users.noreply.github.com>
2025-05-08 15:37:22 -1000

    Update to separate flake from package, preparing for nixpkgs packaging

--- flake.nix
+++ flake.nix
@@ -3,99 +3,42 @@
     nixpkgs = {
       url = "github:nixos/nixpkgs/nixos-unstable";
     };
-    flake-parts.url = "github:hercules-ci/flake-parts";
   };
   outputs =
-    inputs:
-    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = [ "x86_64-linux" ];
-      perSystem =
-        {
-          config,
-          self',
-          pkgs,
-          lib,
-          system,
-          ...
-        }:
+    inputs@{ nixpkgs, self, ... }:
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      eachSystem =
+        f: inputs.nixpkgs.lib.genAttrs systems (system: f inputs.nixpkgs.legacyPackages.${system});
+
+    in
+    {
+      packages = eachSystem (
+        pkgs:
         let
-          runtimeDeps = with pkgs; [
-          ];
-          buildDeps = with pkgs; [
-            gradle
-            makeWrapper
-            pkg-config
-          ];
-          devDeps = with pkgs; [
-            jdk
-            just
-            nushell
-          ];
-
-          javaPackage = pkgs.stdenv.mkDerivation (finalAttrs: {
-            pname = "signum-node";
-            version = "3.8.4";
-
-            src = ./.;
-
-            nativeBuildInputs = buildDeps ++ devDeps;
-
-            mitmCache = pkgs.gradle.fetchDeps {
-              # inherit (finalAttrs) pname;
-              pkg = finalAttrs;
-              data = ./deps.json;
-            };
-
-            __darwinAllowLocalNetworking = true;
-
-            gradleFlags = [
-              "-x test"
-              "-Dfile.encoding=utf-8"
-              "-Pversion=${finalAttrs.version}" # Manually give gradle the version
-            ];
-
-            gradleBuildTask = "shadowJar";
-
-            doCheck = true;
-
-            installPhase = ''
-              mkdir -p $out/{bin,share}
-              cp build/libs/signum-node-$version-all.jar $out/share/signum-node
-
-              makeWrapper ${lib.getExe pkgs.jre} $out/bin/signum-node \
-                --add-flags "-jar $out/share/signum-node"
-            '';
-
-            meta.sourceProvenance = with pkgs.lib.sourceTypes; [
-              fromSource
-              binaryBytecode
-            ];
-          });
-
-          mkDevShell =
-            inputs:
-            pkgs.mkShell {
-              shellHook = '''';
-              LD_LIBRARY_PATH = "${pkgs.stdenv.cc.cc.lib}/lib";
-              buildInputs = runtimeDeps;
-              nativeBuildInputs = buildDeps ++ devDeps ++ [ inputs ];
-            };
+          signumNodeOutputs = pkgs.callPackage ./package.nix { };
         in
-        {
-          _module.args.pkgs = import inputs.nixpkgs {
-            inherit system;
-            overlays = [ ];
-            config.allowUnfreePredicate =
-              pkg:
-              builtins.elem (lib.getName pkg) [
-              ];
-          };
+        rec {
+          default = signumNode;
+          signumNode = signumNodeOutputs.package;
+          updateDeps = signumNodeOutputs.updateDeps.mitmCache.updateScript;
+        }
+      );
 
-          packages.default = javaPackage;
-          packages.updateDeps = javaPackage.mitmCache.updateScript;
-
-          devShells.default = mkDevShell "";
-
-        };
+      devShells = eachSystem (
+        pkgs:
+        let
+          signumNodeOutputs = pkgs.callPackage ./package.nix { };
+        in
+        rec {
+          default = signumDevShell;
+          signumDevShell = signumNodeOutputs.devShell;
+        }
+      );
     };
 }

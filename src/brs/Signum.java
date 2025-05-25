3576bc83f6876af5bf00b4f0c0a0a874bc17ff03
frankTheTank72 <39116221+frankTheTank72@users.noreply.github.com>
2025-05-25 20:22:09 +0200

    Adding Reader

--- src/brs/Signum.java
+++ src/brs/Signum.java
@@ -58,6 +58,7 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.Reader;
 import java.nio.charset.StandardCharsets;
 import java.util.Properties;
 import java.util.concurrent.atomic.AtomicBoolean;

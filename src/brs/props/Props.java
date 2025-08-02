8b31ea9506471683083bbf2374953ac5cd061c7c
frankTheTank72 <39116221+frankTheTank72@users.noreply.github.com>
2025-08-02 13:04:23 +0200

    Remove double entry (#891)

--- src/brs/props/Props.java
+++ src/brs/props/Props.java
@@ -104,7 +104,7 @@ public class Props {
     public static final Prop<String> BRS_CHECKPOINT_HASH = new Prop<>("node.checkPointPrevHash",
             "8f76beca852b41447ac59fbdf78750e43dde97f9fc09ec8f071eeb03c43420f0");
     public static final Prop<String> BRS_PK_CHECKS = new Prop<>("node.pkChecks",
-            "dba639ec3450e0b1;169b3b99ce28a350;a83c47e772a35586;6db77a51a7def19d;c4823aa7028f6735;fb0e32a5bc032257;15a35aa0515e3584;27fcf52c3bc40fba;c4823aa7028f6735;981454e22b5ac976;0cb15471ad76fcd1;");
+            "dba639ec3450e0b1;169b3b99ce28a350;a83c47e772a35586;6db77a51a7def19d;c4823aa7028f6735;fb0e32a5bc032257;15a35aa0515e3584;27fcf52c3bc40fba;981454e22b5ac976;0cb15471ad76fcd1;");
 
     // GPU options
     public static final Prop<Boolean> GPU_ACCELERATION = new Prop<>("GPU.Acceleration", false);
